d916bfde20d3205243d2c4ed5bd78e32c9794bac
Pavel Karenda <korendos@gmail.com>
2020-02-24 19:33:10 +0300

    feat: solved task reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let res = Math.abs(n).toString().split('').reverse();
+    while (res[0] === '0') {
+        res.shift();
+    }
+    return Number(res.join(''));
 }
